2d0d32a9f0496560cdfeb87d32179073bbcda805
o_joh <jack007conan@outlook.jp>
2019-03-19 18:23:19 +0900

    stop関数とstart関数を追加

--- TimeManager/Code/Time/Manager.cpp
+++ TimeManager/Code/Time/Manager.cpp
@@ -1,72 +1,78 @@
 #include"Manager.h"
-#include<Windows.h>
-#pragma comment(lib, "winmm.lib")
+#include<DxLib.h>
 
 using namespace Time;
 
-unsigned int GetNowCount() {
-	return (unsigned int)(timeGetTime());
-}
-
-void Manager::m_update() {
-	unsigned int t = b_now - m_ini - m_stopUnit.getCount();
-	m_dur = t - m_now;
-	if (m_leftStop.elem == 0) {
-		m_now = t;
-	}
-	else if (m_leftStop.getCount() >= m_dur) {
-		m_dur = 0;
-	}
-	else {
-		m_dur -= m_leftStop.getCount();
-		m_now += m_dur;
-		m_stopUnit.elem += m_leftStop.elem;
-		m_leftStop.elem = 0;
-	}
-}
-
 Manager::Manager() :
-	b_now(g_now),
-	m_ini(b_now),
+	m_baseNow(m_g_now),
+	m_ini(m_baseNow),
 	m_now(0),
 	m_dur(0),
-	m_stopUnit(0),
-	m_leftStop(0)
+	m_stopFrame(0),
+	m_leftStopFrame(0)
 {
 	mList.emplace_back(this);
 	mItr = --(mList.end());
 }
 
 Manager::Manager(Manager *manager) :
-	b_now(manager->getNow()),
-	m_ini(b_now),
+	m_baseNow(manager->getNow()),
+	m_ini(m_baseNow),
 	m_now(0),
 	m_dur(0),
-	m_stopUnit(0),
-	m_leftStop(0)
+	m_stopFrame(0),
+	m_leftStopFrame(0)
 {
 	mList.emplace_back(this);
 	mItr = --(mList.end());
 }
 
-
-
 Manager::~Manager() {
 	mList.erase(mItr);
 }
 
+void Manager::m_update() {
+	unsigned int t = m_baseNow - m_ini - mGetCount(m_stopFrame);
+	m_dur = t - m_now;
+	if (m_leftStopFrame == 0) {
+		m_now = t;
+	}
+	else if (mGetCount(m_leftStopFrame) >= m_dur) {
+		m_dur = 0;
+	}
+	else {
+		m_dur -= mGetCount(m_leftStopFrame);
+		m_now += m_dur;
+		m_stopFrame += m_leftStopFrame;
+		m_leftStopFrame = 0;
+	}
+}
+
 void Manager::stop(const unsigned int &stopFrame) {
-	m_leftStop = Unit(stopFrame);
+	m_leftStopFrame = stopFrame;
+}
+
+void Manager::stop() { //挙動未確認
+	m_leftStopFrame = (unsigned int)(-1);
+}
+void Manager::start() { //挙動未確認
+	m_leftStopFrame = 0;
+	m_stopFrame += m_baseNow - m_ini - mGetCount(m_stopFrame) - m_now;
+}
+
+void Manager::init() { //未完成
+	m_ini = m_baseNow;
+
 }
 
 void Manager::update() {
 #ifndef NDEBUG
-	g_dur = 1;
-	g_now++;
+	m_g_dur = 1;
+	m_g_now++;
 #else
-	unsigned int t = (unsigned int)(GetNowCount()) - g_ini;
-	g_dur = t - g_now;
-	g_now = t;
+	unsigned int t = (unsigned int)(GetNowCount()) - m_g_ini;
+	m_g_dur = t - m_g_now;
+	m_g_now = t;
 #endif
 
 	for (auto &i : mList) {
@@ -76,12 +82,12 @@ void Manager::update() {
 
 std::list<Time::Manager*> Manager::mList;
 #ifndef NDEBUG
-const unsigned int Time::Manager::g_ini = 0;
+const unsigned int Time::Manager::m_g_ini = 0;
 #else
-const unsigned int Time::Manager::g_ini = (unsigned int)(GetNowCount());
+const unsigned int Time::Manager::m_g_ini = (unsigned int)(GetNowCount());
 #endif
-unsigned int Time::Manager::g_now = 0;
-unsigned int Time::Manager::g_dur = 0;
+unsigned int Time::Manager::m_g_now = 0;
+unsigned int Time::Manager::m_g_dur = 0;
 
 bool Time::Manager::operator==(const unsigned int &o) {
 	return (m_now - (unsigned int)(o * ((double)(50) / 3)) < m_dur);
--- TimeManager/Code/Time/Manager.h
+++ TimeManager/Code/Time/Manager.h
@@ -2,56 +2,65 @@
 #include<list>
 
 namespace Time {
-
-	class Unit {
+	constexpr unsigned int FrameRate = 60;
+	class Manager {
 	public:
-		Unit(const unsigned int &elem, bool absoluteFlag = false) :
-			elem(elem),
-			absoluteFlag(absoluteFlag)
-		{}
-		unsigned int elem;
-		bool absoluteFlag;
-		unsigned int getCount() {
+		//コンストラクタ等
+		Manager();
+		Manager(Manager *);
+		Manager(const Manager &) = delete; //コピー禁止
+		Manager &operator= (const Manager &) = delete; //コピー禁止
+		~Manager();
+
+	private:
+		//privateメンバ変数
+		const unsigned int &m_baseNow; //上の階層の時間
+		unsigned int m_ini; //初期化時の上の階層の時間
+		unsigned int m_now; //初期化時からの時間(ms)
+		unsigned int m_dur; //前のフレームとの時間差(ms)
+		unsigned int m_stopFrame; //stop関数による上の階層とのズレ(F)
+		unsigned int m_leftStopFrame; //stop関数で指定された値(F)を保存するだけ。再度時間を進め始めたら0になる
+		
+		//メンバ関数
+		void m_update(); //m_now等を変更
+	public:
+		void stop(const unsigned int &stopFrame); //m_leftStopFrameを書き換え
+		void stop(); //startするまでstop
+		void start(); //stopされてたものをstopFrameにかかわらずその地点から動かす
+		void init(); //上の階層は変えずにその他要素を初期値に戻す
+
+	private: 
+		//内部インライン関数
+		unsigned int mGetCount(const unsigned int &frame) const { //絶対時間に変換して返す(Release時のみ)
 #ifndef NDEBUG
-			return elem;
+			return frame;
 #else
-			if (absoluteFlag) return elem;
-			else return (unsigned int)(elem * ((double)50 / 3));
+			return (unsigned int)(frame * ((double)1000 / FrameRate));
 #endif
 		}
-	};
 
-	class Manager {
-	private:
-		const unsigned int &b_now;
-		const unsigned int m_ini;
-		unsigned int m_now;
-		unsigned int m_dur;
-
-		Unit m_stopUnit; //上の階層とのズレ
-		Unit m_leftStop; //stop関数で指定された値を保存するだけ
-		void m_update();
+		//とりあえずの関数
 	public:
 		const auto &getNow() {
 			return m_now;
 		}
 
-		Manager();
-		Manager(Manager *);
-		Manager(const Manager &) = delete;
-		Manager &operator= (const Manager &) = delete;
-		~Manager();
-
-		void stop(const unsigned int &stopFrame);
-
+	public:
+		//staticなメンバ関数
 		static void update();
-		static unsigned int g_now;
-		static const unsigned int g_ini;
-		static unsigned int g_dur;
+
 	private:
+		//staticなメンバ変数
+		static unsigned int m_g_now;
+		static const unsigned int m_g_ini;
+		static unsigned int m_g_dur;
+
+		//update関数関連
 		static std::list<Time::Manager*> mList;
 		std::list<Time::Manager*>::iterator mItr;
+
 	public:
+		//operator
 		bool operator==(const unsigned int &o);
 		bool operator!=(const unsigned int &o) {
 			return !((*this) == o);
